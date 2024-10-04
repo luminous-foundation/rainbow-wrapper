38b31b4d9d9a24e48a083df937db02af3d0322cf
grimtin10 <grimtin10@gmail.com>
2024-10-03 21:45:51 -0600

    add imports

--- src/rainbow_wrapper/wrapper.rs
+++ src/rainbow_wrapper/wrapper.rs
@@ -5,11 +5,12 @@ use super::{conversions::{to_bytecode_string, to_immediate, to_types}, types::{T
 pub struct Wrapper {
     pub bytes: Vec<u8>,
     pub strings: HashSet<String>,
+    pub imports: HashSet<String>,
 }
 
 impl Wrapper {
     pub fn new() -> Wrapper {
-        Wrapper { bytes: Vec::new(), strings: HashSet::new() }
+        Wrapper { bytes: Vec::new(), strings: HashSet::new(), imports: HashSet::new() }
     }
 
     pub fn push_bytes(&mut self, mut bytes: Vec<u8>) {
@@ -20,17 +21,28 @@ impl Wrapper {
         self.strings.insert(string.to_string());
     }
 
+    pub fn push_import(&mut self, import: &String) {
+        self.imports.insert(import.to_string());
+    }
+
     pub fn emit(&mut self) -> Vec<u8> {
         let mut res: Vec<u8> = Vec::new();
 
+        for import in self.imports.iter() {
+            res.push(0xFA);
+            res.append(&mut to_bytecode_string(import));
+        }
+
         res.append(&mut self.bytes);
 
-        res.push(0xFC);
-        for str in self.strings.iter() {
-            res.append(&mut to_bytecode_string(str));
-            res.append(&mut to_types(&vec![Type::POINTER, Type::U8]));
-            res.append(&mut to_immediate(&Types::UNSIGNED(str.len() as u64)));
-            res.append(&mut to_bytecode_string(str));
+        if self.strings.len() > 0 {
+            res.push(0xFC);
+            for str in self.strings.iter() {
+                res.append(&mut to_bytecode_string(str));
+                res.append(&mut to_types(&vec![Type::POINTER, Type::U8]));
+                res.append(&mut to_immediate(&Types::UNSIGNED(str.len() as u64)));
+                res.append(&mut to_bytecode_string(str));
+            }
         }
 
         return res;
