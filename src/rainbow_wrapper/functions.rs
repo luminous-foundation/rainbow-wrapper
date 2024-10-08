d6e8c6bf3f526dd23d02d44541b87ab7f61741f3
grimtin10 <grimtin10@gmail.com>
2024-10-08 17:05:37 -0600

    document functions.rs

--- src/rainbow_wrapper/functions.rs
+++ src/rainbow_wrapper/functions.rs
@@ -6,6 +6,15 @@ pub struct Arg {
     pub typ: Vec<Type>
 }
 
+/// Returns the bytes for a function.
+/// 
+/// `name`: name of the function
+/// 
+/// `args`: arguments of the function
+/// 
+/// `return_type`: return type of the function
+/// 
+/// `body`: body of the function, in bytecode
 pub fn generate_function(name: &String, args: &Vec<Arg>, return_type: &Vec<Type>, body: &Vec<u8>) -> Vec<u8> {
     let mut res: Vec<u8> = Vec::new();
 
@@ -25,6 +34,9 @@ pub fn generate_function(name: &String, args: &Vec<Arg>, return_type: &Vec<Type>
     return res;
 }
 
+/// Returns the bytes for a scope with the body `body`.
+/// 
+/// `body`: body of the scope, in bytecode
 pub fn generate_scope(body: &Vec<u8>) -> Vec<u8> {
     let mut res: Vec<u8> = Vec::new();
 
