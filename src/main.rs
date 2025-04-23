9193981c1e5b60083065589a6a39552f69c3972f
grimtin10 <grimtin10@gmail.com>
2025-04-23 15:59:30 -0600

    add chunk creation methods

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use rainbow_wrapper::Wrapper;
 
 pub fn main() {
     let mut test = Wrapper::new();
+    test.code_begin();
+
+    // add instructions n shit
+
+    test.code_end();
 
     println!("{:?}", test.emit())
 }
